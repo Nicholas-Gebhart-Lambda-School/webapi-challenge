1c0c9aee836304327dae6f46bfcdbcf8a8e86439
unknown <nicholas.gebhart@gmail.com>
2019-11-01 08:56:39 -0700

    feat: add projects post

--- actions.routes.js
+++ actions.routes.js
@@ -1,8 +1,27 @@
 const router = require("express").Router();
 const actions = require("./data/helpers/actionModel");
+const projects = require("./data/helpers/projectModel");
 
 router.get("/", (req, res) => {
   actions.get().then(actions => res.status(200).json(actions));
+});
+
+router.post("", (req, res) => {});
+
+router.put("/:id", (req, res) => {});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  actions
+    .remove(id)
+    .then(action => {
+      if (action) {
+        res.status(200).json({ res: `removed ${action} record` });
+      } else {
+        res.status(400).json({ err: "that id does not exist" });
+      }
+    })
+    .catch(() => res.status(500).json({ err: "server error" }));
 });
 
 module.exports = router;
--- projects.routes.js
+++ projects.routes.js
@@ -1,3 +1,26 @@
 const router = require("express").Router();
+const projects = require("./data/helpers/projectModel");
+const actions = require("./data/helpers/actionModel");
+const ner = { err: "There has been a server error" };
+
+router.get("/", (req, res) => {
+  projects.get().then(projects => res.status(200).json(projects));
+});
+
+router.post("/", (req, res) => {
+  const { name, description } = req.body;
+  const completed = req.body.completed || false;
+  const newProject = { name, description, completed };
+  if (!name || !description) {
+    res.status(400).json({ err: "name and description are required" });
+  } else {
+    projects
+      .insert(newProject)
+      .then(project => res.status(200).json(project))
+      .catch(() => {
+        res.status(500).json(ner);
+      });
+  }
+});
 
 module.exports = router;
